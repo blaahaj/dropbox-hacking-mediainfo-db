fc02a613272785d95512acb97a5dcec54fd79c6e
Rachel Evans <rachel@rachelevans.org>
2025-04-26 18:21:38 +0200

    chore: loosen commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,1 +1,10 @@
-export default { extends: ["@commitlint/config-conventional"] };
+import { RuleConfigSeverity, UserConfig } from "@commitlint/types";
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 200],
+  },
+};
+
+export default config;
